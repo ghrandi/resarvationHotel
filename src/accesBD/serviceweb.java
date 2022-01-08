2f04ddb9b08c33751e8f49764108a3d366cb9578
ghrandi <93522718+ghrandi@users.noreply.github.com>
2022-01-08 18:07:43 +0100

    Create serviceweb.java
    je suis desole monsieur maven ne fonctione pas comme il faut dans mon pc

---
+++ src/accesBD/serviceweb.java
@@ -0,0 +1,51 @@
+package accesBD;
+
+import java.util.List;
+import entites.*;
+//import javax.ws.rs.*;
+import java.sql.SQLException;
+
+//@Path("client")
+
+
+public class serviceweb {
+//http://localhost:8005/reservationhotel/webapi/client/indetifiant/11235654
+//@GET
+    //@Path("identifiant/{cin}")
+    //@Produces("application/json")
+    //public Client findClient(@PathParam("cin") String cin) throws SQLException{
+    //ClientDAO pdao=new ClientDAO();
+    //System.out.println("cin");
+        //return pdao.FindById("cin") ;
+       
+        }
+	
+		//clientDao client=new clientDao();
+ //http://localhost:8005/reservationhotel/webapi/client/create?nom=wajdi&cin=111056895&numtel=26585241&typepaiment=cash
+		//@POST
+		//@Path("create")
+		//public void newClient(@QueryParam("nom") String nom, @QueryParam("cin") String cin,@QueryParam("numtel") String numtel,
+		//@QueryParam("typepaiment") String typepaiment) {
+		//clientDao ClientDAO = new clientDao();
+		//ClientDAO.ajouterclient(new Client(nom, cin,numtel,typepaiment));
+		
+		//}
+	//}
+	//http://localhost:8005/reservationhotel/client/webapi/client/delete/22544552
+	//@DELETE
+	//@Path("delete/{cin}")
+	//@Produces(MediaType.TEXT_PLAIN)
+	//public void SupprimerCompte(@PathParam("Cin") String Cin) {
+
+	//clientDao.delete(cin);
+	//}
+	//http://localhost:8005/reservationhotel/webapi/client/edit/2222222?numtel=26236859
+	//@PUT
+
+	//@Path("edit/{cin}")
+
+	//@Produces({ MediaType.TEXT_PLAIN })
+	//public void updateclient( @PathParam("cin") String cin,@QueryParam("numtel") String adresse) {
+	//clientDao.updateclient(cin);
+	//}
+//}
